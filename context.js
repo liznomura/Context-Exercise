4273a2e1e24c3080f4c1d261a4d6f5033d7ebb7f
liznomura <lizn@hawaii.edu>
2017-06-22 15:07:06 -1000

    completed

--- context.js
+++ context.js
@@ -1,7 +1,7 @@
 var Robot = function (name) {
   this.name = name;
 
-}
+};
 
 function add ( op1, op2 ) {
   this.name = this.name || "Humans";
@@ -16,15 +16,17 @@ var bmo = new Robot("B-Mo");
 var wall_e = new Robot("Wall-E");
 
 
-// #1
-console.log(  );
 
-// #2  
-console.log(  );
+// #1
+console.log(add(0,1));
+
+// #2
+console.log(add.call(voltron, 2, 3));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus, [20, 30]));
 
 // #4
+var calculate = add.bind(bender);
 
-console.log(  );
+console.log(calculate('drinking ', 'beer'));
